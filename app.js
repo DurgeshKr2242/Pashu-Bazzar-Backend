200572b813384b999941af0b30b6b1c371fb82ee
DurgeshKr2242 <code.durgesh@gmail.com>
2021-11-26 16:27:08 +0530

    cors changed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 // const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
+var cors = require("cors");
 
 const postsRoutes = require("./routes/postsRoutes");
 const usersRoutes = require("./routes/usersRoutes");
@@ -10,16 +11,17 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-  next();
-});
+//   next();
+// });
+app.use(cors());
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", usersRoutes);
