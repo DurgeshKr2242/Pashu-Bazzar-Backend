5f1d0817370bcf8d769836183419fe7bc7e8ad72
DurgeshKr2242 <code.durgesh@gmail.com>
2021-11-26 16:31:34 +0530

    trial 69

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ const app = express();
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-//   next();
-// });
-app.use(cors());
+  next();
+});
+// app.use(cors());
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", usersRoutes);
