61ad38849ed07be581864c3124c158594280c6bc
DurgeshKr2242 <code.durgesh@gmail.com>
2021-11-27 18:05:05 +0530

    random

--- app.js
+++ app.js
@@ -9,19 +9,21 @@ const usersRoutes = require("./routes/usersRoutes");
 const HttpError = require("./models/httpError");
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    "access-control-allow-origin"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+//     "access-control-allow-origin"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-  next();
-});
+//   next();
+// });
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", usersRoutes);
